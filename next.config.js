edc41c3749c0105ac1931efdd796e8fa6993ff97
Triggered Legend <179379646+TriggeredLegend@users.noreply.github.com>
2025-06-15 10:33:49 +0600

    Fixed next.config.js to handle both development and production environments

--- next.config.js
+++ next.config.js
@@ -48,7 +48,7 @@ const securityHeaders = [
   },
 ]
 
-const output = process.env.EXPORT ? 'export' : undefined
+const output = undefined
 const basePath = process.env.BASE_PATH || undefined
 const unoptimized = !!process.env.UNOPTIMIZED
 
