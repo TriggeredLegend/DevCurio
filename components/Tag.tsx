3adb301df86337188c6e7b78b0f1bb3c402d6a17
Triggered Legend <179379646+TriggeredLegend@users.noreply.github.com>
2025-06-25 00:55:15 +0600

    Fixed Accessibility Issues in Tag Component.

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { slug } from 'github-slugger'
+
 interface Props {
   text: string
 }
@@ -8,7 +9,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase"
+      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 focus:ring-primary-500 mr-3 inline-flex min-h-[48px] min-w-[48px] items-center justify-center rounded px-2 py-1 text-sm font-medium uppercase focus:ring-2 focus:ring-offset-2 focus:outline-none"
     >
       {text.split(' ').join('-')}
     </Link>
