0dff880a3f20f19a7aa96e18798efef995a9114c
Ihsan Ahmad Ayaan <179379646+TriggeredLegend@users.noreply.github.com>
2025-12-17 18:53:53 +0600

    Update PteroCore project details and link

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,15 +8,9 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'PteroCore',
-    description: `PteroCore is an AI addon for Pterodactyl that lets users manage Minecraft servers using simple text commands—no tech skills needed. Fast, secure, and effortless.`,
+    description: `We designed PteroCore to be premium, budget-friendly, and suitable for server owners, tech-savvy individuals, and businesses.`,
     imgSrc: '/static/images/pterocore.png',
-    href: 'https://www.devcurio.com/projects',
-  },
-  {
-    title: 'CurioHost',
-    description: `CurioHost is a fast and reliable hosting platform built for developers and creators. From game servers to web apps, it offers simple, scalable solutions with full control and no hassle.`,
-    imgSrc: '/static/images/curiohost.png',
-    href: 'https://host.devcurio.com/',
+    href: 'https://pterocore.space/',
   },
 ]
 
